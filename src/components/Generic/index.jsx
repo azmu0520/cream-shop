266f21af6c37775c1d9e301b049a4c4589e5c89e
azmu0520 <azizbekbootcam@gmail.com>
2022-07-31 16:11:27 +0500

    doner

--- src/components/Generic/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-const Generic = () => {
-  const location = useLocation();
-
-  return <div>{location.pathname}: is comming soon</div>;
-};
-
-export default Generic;
